0fd6058ccf79074d50bebbba045557bf66372865
Guilherme Braz <guilhermebrazmoreirafaria@gmail.com>
2021-11-24 16:39:08 -0300

    feature: criando rotas e components

---
+++ src/routes/index.jsx
@@ -0,0 +1,10 @@
+import { Route, Switch } from "react-router";
+
+function Routes() {
+  return (
+    <Switch>
+      <Route exact path="/"></Route>
+      <Route path="/"></Route>
+    </Switch>
+  );
+}
